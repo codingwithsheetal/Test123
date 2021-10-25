b721415616fc58de3db58bce6153bfa62c2c0873
sheetal <sheetalacharya@gmail.com>
2021-10-24 19:37:08 -0700

    Initial Commit

---
+++ Test123/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  Test123
+//
+//  Created by Sheetal Acharya on 10/24/21.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
